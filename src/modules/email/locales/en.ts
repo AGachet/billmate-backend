3072ec3289507d6aefa5de5c1103c64ca14e7d72
AnthoGac <anthony.gachet@gmail.com>
2025-04-21 22:52:33 +0200

    feat(#62): add people table & create people on first signin

--- src/modules/email/locales/en.ts
+++ src/modules/email/locales/en.ts
@@ -2,6 +2,7 @@ export const en = {
   accountConfirmation: {
     subject: 'Confirm your BillMate account',
     title: 'Welcome to BillMate!',
+    greeting: 'Hello',
     body: 'Thank you for signing up to BillMate. To activate your account, please click the button below:',
     button: 'Activate my account',
     fallback: "If the button doesn't work, you can copy and paste the following link into your browser:",
@@ -11,6 +12,7 @@ export const en = {
   passwordReset: {
     subject: 'Reset your BillMate password',
     title: 'Password Reset',
+    greeting: 'Hello',
     body: 'You have requested to reset your password. Click the button below to create a new one:',
     button: 'Reset my password',
     fallback: "If the button doesn't work, you can copy and paste the following link into your browser:",
