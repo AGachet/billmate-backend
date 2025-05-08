d3abea9641e51b2dcd9ee59f3e7ef0b0862dd5a0
AnthoGac <anthony.gachet@gmail.com>
2025-05-08 18:37:12 +0200

    feat(#64): add organization module

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,19 +8,33 @@ import { APP_FILTER } from '@nestjs/core'
  * Dependencies
  */
 import { GlobalExceptionFilter } from '@common/filters/global-exception.filter'
+import { AccountAccessModule } from '@common/services/account-access/account-access.module'
 import { LoggerModule } from '@common/services/logger/logger.module'
 import { EnvModule } from '@configs/env/env.module'
 import { PrismaModule } from '@configs/prisma/prisma.module'
 import { AccountsModule } from '@modules/accounts/accounts.module'
 import { ApiDocsModule } from '@modules/api-docs/api-docs.module'
 import { AuthModule } from '@modules/auth/auth.module'
+import { EntitiesModule } from '@modules/entities/entities.module'
 import { HealthModule } from '@modules/health/health.module'
+import { OrganizationsModule } from '@modules/organizations/organizations.module'
 
 /**
  * Declaration
  */
 @Module({
-  imports: [...(process.env.NODE_ENV === 'development' ? [ApiDocsModule] : []), HealthModule, PrismaModule, AuthModule, LoggerModule, EnvModule, AccountsModule],
+  imports: [
+    ...(process.env.NODE_ENV === 'development' ? [ApiDocsModule] : []),
+    HealthModule,
+    PrismaModule,
+    AuthModule,
+    LoggerModule,
+    EnvModule,
+    AccountAccessModule,
+    AccountsModule,
+    OrganizationsModule,
+    EntitiesModule
+  ],
   controllers: [],
   providers: [
     {
